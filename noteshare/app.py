84956ed269d5ce5cae215b5c2b5f316f3a5ff81e
formal-acc <60254372+formal-acc@users.noreply.github.com>
2020-05-23 04:04:33 +0530

    Added upload feature.
    A form that supports upload is completed but link specific upload is yet to be achieved.

--- noteshare/app.py
+++ noteshare/app.py
@@ -2,20 +2,23 @@ import os
 from flask import Flask, flash, request, redirect, url_for, render_template, request, jsonify
 from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, FileField
-from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = '/path/to/the/uploads'
-ALLOWED_EXTENSIONS = {'zip', 'gz', 'pdf', 'png', 'jpg', 'jpeg'}
+from flask_uploads import UploadSet, configure_uploads, IMAGES, DOCUMENTS, ARCHIVES
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+folders = UploadSet('folders', IMAGES + ARCHIVES + DOCUMENTS, default_dest=lambda x: 'folders')
+
+app.config['UPLOAD_FOLDER'] =  './'
+configure_uploads(app, folders)
+
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #maximum size of the file
 app.config['SECRET_KEY'] = 'secret'
 
 class CourseForm(FlaskForm):
-	#create a db to handle the courses and semesters
-	sem = SelectField('Semester', choices=[('M19', 'Monsoon19'), ('S19', 'Spring19')])
+	#create a db to handle the courses, years and semesters
+	sem = SelectField('Year', choices=[('M19', 'Monsoon19'), ('S19', 'Spring19')])
 	course = SelectField('Course', choices=[('A', 'Course A'), ('B', 'Course B')])
+	upload_file = FileField()
 
 @app.route('/upload', methods = ['GET', 'POST'])
 def formdisp():
@@ -23,12 +26,13 @@ def formdisp():
 
 	if form.validate_on_submit():
 		
-		#use the form data as path in local storage
-		#do this using form.sem.data
+		filename = folders.save(form.upload_file.data)
 
+		flash("Saved!")
 
-		return render_template('upload.html', form=form)
-
+	#customise "folders"
+	#use the form data as path in local storage
+	#do this using form.sem.data
 
 	#ignore the next line completely please
 	# form.city.choices = [(city.id, city.name) for city in City.query.filter_by(state='CA').all()] 
@@ -39,6 +43,3 @@ def formdisp():
 
 if __name__ == "__main__":
 	app.run(debug=True)
-	
-
-
