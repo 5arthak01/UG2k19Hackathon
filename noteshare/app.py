0a0a9f36088a4df6beff5691c97901f89f3c8605
Sarthak Agrawal <60254372+formal-acc@users.noreply.github.com>
2020-05-24 19:37:49 +0530

    Update app.py

--- noteshare/app.py
+++ noteshare/app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, flash, request, redirect, url_for, render_template, request, jsonify, send_from_directory, abort
 from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
-from wtforms_sqlalchemy.fields import QuerySelectField
 from wtforms import SelectField, StringField, FileField
 from flask_uploads import UploadSet, configure_uploads, IMAGES, TEXT, DOCUMENTS, ARCHIVES
 from flask_bootstrap import Bootstrap
@@ -17,31 +16,26 @@ configure_uploads(app, folders)
 
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #maximum size of the file
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/courses.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/finaldb.sqlite'
 app.config['SECRET_KEY'] = 'secret'
 
 db = SQLAlchemy(app)
 
 class Courses(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	sem_id = db.Column(db.String(20), nullable=False) # M19 Might be redundant
 	sem_name = db.Column(db.String(80)) # Monsoon19
-	course_id = db.Column(db.String(20)) #DSA Might be redundant
 	course_name = db.Column(db.String(80)) #Data Str and Algo
 
 class CourseForm(FlaskForm):
 	sem = SelectField('Year', choices=[(sem[0],sem[0]) for sem in sorted(set(Courses.query.with_entities(Courses.sem_name)), reverse=True) ] )
-	course = SelectField('Course', choices=[])
-	info = SelectField('Info', choices=[('Notes','Notes'), ('End-sem', 'End-sem'), ('Mid-sem', 'Mid-sem'), ('Quiz', 'Quiz'), ('Miscellaneous', 'Miscellaneous')])
+	course = SelectField('Course', choices=[(course[0],course[0]) for course in set(Courses.query.with_entities(Courses.course_name))])
 	upload_file = FileField()
+
 
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload():
 	form = CourseForm()
-	form.course.choices = [ (course.course_name, course.course_name) for course in Courses.query.filter_by(sem_name='Spring20').all() ]
-
 	if form.validate_on_submit():
-
 		# customise file path in local directory according to the data entered
 		file_path = '../'+ str(form.sem.data) + '/' + str(form.course.data)
 
@@ -54,28 +48,28 @@ def upload():
 	
 	return render_template('upload.html', form=form)
 
-
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/browse', methods = ['GET', 'POST'])
 def browse():
-	form = CourseForm()
-	form.course.choices = [ (course.course_name, course.course_name) for course in Courses.query.filter_by(sem_name='Spring20').all() ]
-
 	retdiv = []
+		
+	file_path = os.getcwd() +'/storage/'
 
-	if form.validate_on_submit():
+	filedirs = os.listdir(file_path) #access all year dirs
 
-		# customise file path in local directory according to the data entered
-		xxtencion =  str(form.sem.data) + '/' + str(form.course.data)
-		file_path = os.getcwd() +'/storage/' + xxtencion
+	dirs=[]
+	for each_yr in filedirs: #access all course dirs
+		courses_in_yr = os.listdir(file_path + '/' +each_yr) 
+		for each_course in courses_in_yr:
+			dirs.append([each_yr, each_course])
 
-		files = os.listdir(file_path)
+	for each_dir in dirs:
+		all_files = os.listdir(file_path + '/' + each_dir[0] + '/' + each_dir[1]) #list of files in second tier dir
+		xxtencion =  each_dir[0] + '/' + each_dir[1]
+		for each_file in all_files:
+			retdiv.append( ( each_dir[0], each_dir[1] ,str(url_for('browse') + '/' + xxtencion+ '/' +each_file), each_file) )
 
-		for each_file in files:
-			
-			retdiv.append( ( str(url_for('browse') + '/' + xxtencion+ '/' +each_file), each_file) )
-
-	return render_template('browse.html', form=form, filelist=retdiv)
+	return render_template('browse.html', filelist=retdiv)#, form=form
 
 @app.route('/browse/<sem>/<course>/<filename>', methods = ['GET', 'POST'])
 def filedisp(sem, course, filename):
@@ -85,6 +79,7 @@ def filedisp(sem, course, filename):
 		return send_from_directory(file_path, filename)
 	except FileNotFoundError:
 		abort(404)
+
 
 
 @app.route('/course/<sem>')
